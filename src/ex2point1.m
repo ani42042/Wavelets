03cf4cf4305f0100fc845cf6a09378d6d0c78353
Arnav Thakur <arnav.thakur@student.kuleuven.be>
2023-12-12 15:30:03 +0100

    added report.tex from overleaf, to enable offline report writing

--- src/ex2point1.m
+++ src/ex2point1.m
@@ -5,8 +5,7 @@ t = linspace(-2, 2, 1000);
 y = abs(t) .*(2+cos(t)) .* sign(t);
 % Try a non-smooth function also:
 % y = abs(t) .* exp(t)
-
-%% decontruct signal
+%% decontruct signal, question 2.1
 
 % Compute its wavelet transform, four levels deep, using the Daubechies 2
 % wavelet.
@@ -20,22 +19,79 @@ semilogy(abs(c));
 xlabel("$i$",Interpreter="latex");
 ylabel("$|c_i|$",Interpreter="latex");
 
-%% recontruct signal
+%% recontruct signal, task 2.2
+
+% apply wavelet transform on signal with added noise
+rng(42)
+epsilon = 1e-1;
+ynoise = y + epsilon*rand(size(y));
+[cnoise,lnoise] = wavedec(ynoise, 5, 'db2');
+
+figure
+plot(t,y)
+hold on
+plot(t,ynoise)
+hold off
+xlabel("$t$",Interpreter="latex");
+ylabel("function",Interpreter="latex");
+legend("$f(t)$","$f(t)+\epsilon \mathcal{U}(0,1)$",Interpreter="latex")
 
 % Find small coefficents and set them to zero.
-T = max(abs(c));
-I = find(abs(c) < 1e-4*T);
-c(I) = 0;
+%T = max(abs(cnoise));
+%delta = 10e-4*T;
+delta = 1e-1;
+
+% change here what thresholding method wanted
+[cnoise,I] = Hard_threshold(delta,cnoise);
+%[cnoise,I] = Soft_threshold(delta,cnoise);
 
 % How many coefficients did we put to zero:
 length(I)
 % out of a total of
-length(c)
+length(cnoise)
+% ratio
+length(I)/length(cnoise)
+errCoeff = abs(c-cnoise);
+
+% coefficent error between real and thresholded noisy
+figure
+semilogy(errCoeff);
+xlabel("$i$",Interpreter="latex");
+ylabel("$|c_i-\hat{c}_i|$",Interpreter="latex");
 
 % Reconstruct the signal
-y2 = waverec(c, l, 'db2');
-
+y2 = waverec(cnoise, lnoise, 'db2');
 % Plot the error on a logarithmic scale. Experiment with the threshold
 % above and see what the effect is
+err = abs(y-y2);
+noise = mean(abs(y-ynoise));
+errTotal = norm(err)
+errTotalnoise = norm(abs(y-ynoise))
 figure
-semilogy(t, abs(y-y2))
+semilogy(t, err)
+hold on
+%semilogy(t, noise)
+yline(noise,Label="noise",Interpreter="latex")
+hold off
+xlabel("$t$",Interpreter="latex");
+ylabel("Errors",Interpreter="latex");
+legend("$|f(t_i)-\hat{f}(t_i)|$",Interpreter="latex")
+
+%% Question 2.3
+
+deltaList = linspace(1e-3,1e-10);
+
+for i = 1:length(deltaList)
+    
+end
+
+function [c,I] = Hard_threshold(delta, c)
+    I = find(abs(c) < delta);
+    c(I) = 0;
+end
+
+function [c, I] = Soft_threshold(delta,c)
+    I = find(abs(c) < delta);
+    c = sign(c).*(abs(c)-delta);
+    c(I) = 0;
+end
