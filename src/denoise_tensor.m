e35b653ba4350553f3ca894a5c418331087d4e36
Arman <arman.mohamedyassin@stedin.net>
2023-12-16 10:06:20 +0100

    generalised denoise function to take wavelet and threshold as args

--- src/denoise_tensor.m
+++ src/denoise_tensor.m
@@ -1,12 +1,11 @@
-function [B1,B2,B3] = denoise_tensor(A1,A2,A3)
-    [c1,l1] = wavedec2(A1, 4, 'bior4.4');
-    [c2,l2] = wavedec2(A2, 4, 'bior4.4');
-    [c3,l3] = wavedec2(A3, 4, 'bior4.4');
+function [B1,B2,B3] = denoise_tensor(A1,A2,A3,wavelet,p)
+    [c1,l1] = wavedec2(A1, 4, wavelet);
+    [c2,l2] = wavedec2(A2, 4, wavelet);
+    [c3,l3] = wavedec2(A3, 4, wavelet);
     
     % Compress by putting small coefficients to zero.
     % Experiment with the threshold p.
     T = max([max(abs(c1)), max(abs(c2)), max(abs(c3))]);
-    p = 1e-2;
     threshold = p*T;
     I1 = find(abs(c1) < threshold);
     c1(I1) = 0;
@@ -15,7 +14,7 @@ function [B1,B2,B3] = denoise_tensor(A1,A2,A3)
     I3 = find(abs(c3) < threshold);
     c3(I3) = 0;
     
-    B1 = waverec2(c1, l1, 'bior4.4');
-    B2 = waverec2(c2, l1, 'bior4.4');
-    B3 = waverec2(c3, l1, 'bior4.4');
+    B1 = waverec2(c1, l1, wavelet);
+    B2 = waverec2(c2, l1, wavelet);
+    B3 = waverec2(c3, l1, wavelet);
 end
