ce902808045788c92886293b75cda3ee30683f74
Arnav Thakur <arnav.thakur@student.kuleuven.be>
2023-12-17 15:27:55 +0100

    added code for testing, 2h of running nessecary

--- src/ex2point2.m
+++ src/ex2point2.m
@@ -6,7 +6,7 @@ figure
 imshow(A)
 
 %% compress image
-p = 1e-3; thresholdType = 'Hard'; waveletType = 'bior4.4';
+p = 1e-2; thresholdType = 'Hard'; waveletType = 'bior4.4';
 A_noise = A;
 [A2,mserr,mserr_rel,compression_ratio,threshold,SNR] = denoising_image(A_noise,p,thresholdType,waveletType,A);
 
@@ -24,18 +24,67 @@ rng(42)
 A_noise = imnoise(A,"gaussian");
 
 msNoise = sqrt(sum(sum(sum( (A_noise-A).^2 ))) / length(A(:)));
-
 % Plot the noisy image
 figure
 imshow(A_noise)
 
 %% Denoise image
-p = 5e-3; thresholdType = 'Soft'; waveletType = 'db1';%'bior4.4'; 
+p = 5e-3; thresholdType = 'Hard'; waveletType = 'db6';%'bior4.4'; 
 [A2,mserr,mserr_rel,compression_ratio,threshold,SNR] = denoising_image(A_noise,p,thresholdType,waveletType,A);
 
 % Plot the result after denosing/compression
 figure
 imshow(A2)
+
+%% computing the best parameters for denosing this image
+wavelets = ["bior1.1", "bior1.3", "bior1.5","bior2.2", "bior2.4",...
+            "bior2.6", "bior2.8","bior3.1","bior3.3", "bior3.5",...
+            "bior3.7","bior3.9", "bior4.4", "bior5.5", "bior6.8",...
+            "rbio1.1", "rbio1.3", "rbio1.5","rbio2.2", "rbio2.4",...
+            "rbio2.6", "rbio2.8","rbio3.1", "rbio3.3", "rbio3.5",...
+            "rbio3.7","rbio3.9", "rbio4.4", "rbio5.5", "rbio6.8"];
+
+tempStrings = strings(0);
+for i = 1:45
+    tempStrings(i) = strcat("db",num2str(i));
+end
+
+% the parameters to be tested
+wavelets = [wavelets, tempStrings];
+thesholding = ["Hard","Soft"];
+p = linspace(0,1,100);
+
+% parameters to store results
+mserrMat = zeros(length(thesholding),length(wavelets),length(p));
+ThreshMat = zeros(length(thesholding),length(wavelets),length(p));
+mprev = +inf;
+BestImage = A_noise;
+for i = 1:length(thesholding)
+    thesholdType = threshold(i);
+    progressbar
+    for j = 1:length(wavelets)
+        wave = wavelets(j);
+        for k = 1:length(p)
+            delta = p(k);
+            [A2,mserr,~,~,threshold,~] = denoising_image(A_noise,delta,thresholdType,wave,A);
+            mserrMat(i,j,k) = mserr;
+            ThreshMat(i,j,k) = threshold;
+            if (mprev > mserr)
+                BestImage = A2;
+            end
+            mprev = mserr;
+        end
+        progressbar(j/length(wavelets));
+    end
+end
+
+%% plot best image
+
+[BestErr,I] = max(mserrMat,[],'all');
+BestThresh = ThreshMat(I);
+% Plot the result after denosing for best params
+figure
+imshow(BestImage)
 
 function [c,I] = Hard_threshold(delta, c)
     I = find(abs(c) < delta);
@@ -89,8 +138,8 @@ function [A2,mserr,mserr_rel,compression_ratio,threshold,SNR] = denoising_image(
         [c3,I3] = Soft_threshold(threshold, c3);
     end
     A2red = waverec2(c1, l1, waveletType);
-    A2green = waverec2(c2, l1, waveletType);
-    A2blue = waverec2(c3, l1, waveletType);
+    A2green = waverec2(c2, l2, waveletType);
+    A2blue = waverec2(c3, l3, waveletType);
     A2 = zeros(size(A));
     A2(:,:,1) = A2red;
     A2(:,:,2) = A2green;
