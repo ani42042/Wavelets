15012d83a241ef73d77eec29a0a4344af0f348b9
Arnav Thakur <arnav.thakur@student.kuleuven.be>
2023-12-18 18:41:42 +0100

    did Task 2.6, doing Q2.7

--- src/denoise_tensor.m
+++ src/denoise_tensor.m
@@ -1,41 +1,48 @@
-function [B1,B2,B3] = denoise_tensor(A1,A2,A3,wavelet,p,type,threshold_type)
-    if contains(type, "redundant")
-        [c1,l1,a1,b1] = swt2(A1, 4, 'bior4.4');
-        [c2,l2,a2,b2] = swt2(A2, 4, 'bior4.4');
-        [c3,l3,a3,b3] = swt2(A2, 4, 'bior4.4');
-        T = max([max(abs(c1)), max(abs(c2)), max(abs(c3))]);
+function [B1,B2,B3,threshold] = denoise_tensor(A1,A2,A3,wavelet,p,type,threshold_type,Level)
+    if nargin < 8
+        Level = 4;
+    end
+
+    if contains(type, "redundant",'IgnoreCase',true)
+        [c1,l1,a1,b1] = swt2(A1, Level, wavelet);
+        [c2,l2,a2,b2] = swt2(A2, Level, wavelet);
+        [c3,l3,a3,b3] = swt2(A3, Level, wavelet);
+        T = max([max(abs(c1),[],"all"), max(abs(c2),[],'all'), max(abs(c3),[],'all')]);
         threshold = p*T;
-        if contains(threshold_type, "hard")
-            [c1,I1] = Hard_threshold(threshold,c1);
-            [c2,I2] = Hard_threshold(threshold,c2);
-            [c3,I3] = Hard_threshold(threshold,c3);
+        sz = [1,numel(c1)]; sz2 = size(c1);
+        c1 = reshape(c1,sz); c2 = reshape(c2,sz); c3 = reshape(c3,sz);
+        if contains(threshold_type, "hard",'IgnoreCase',true)
+            [c1,~] = Hard_threshold(threshold,c1);
+            [c2,~] = Hard_threshold(threshold,c2);
+            [c3,~] = Hard_threshold(threshold,c3);
         else
-            [c1,I1] = Soft_threshold(threshold,c1);
-            [c2,I2] = Soft_threshold(threshold,c2);
-            [c3,I3] = Soft_threshold(threshold,c3);
+            [c1,~] = Soft_threshold(threshold,c1);
+            [c2,~] = Soft_threshold(threshold,c2);
+            [c3,~] = Soft_threshold(threshold,c3);
         end
-        B1 = iswt2(c1, l1,a1,b1, wavelet);
-        B2 = iswt2(c2, l2,a2,b2, wavelet);
-        B3 = iswt2(c3, l3,a3,b3, wavelet);
+        c1 = reshape(c1,sz2); c2 = reshape(c2,sz2); c3 = reshape(c3,sz2);
+        B1 = iswt2(c1,l1,a1,b1, wavelet);
+        B2 = iswt2(c2,l2,a2,b2, wavelet);
+        B3 = iswt2(c3,l3,a3,b3, wavelet);
     else
-        [c1,l1] = wavedec2(A1, 4, wavelet);
-        [c2,l2] = wavedec2(A2, 4, wavelet);
-        [c3,l3] = wavedec2(A3, 4, wavelet);
-        T = max([max(abs(c1)), max(abs(c2)), max(abs(c3))]);
+        [c1,l1] = wavedec2(A1, Level, wavelet);
+        [c2,l2] = wavedec2(A2, Level, wavelet);
+        [c3,l3] = wavedec2(A3, Level, wavelet);
+        T = max([max(abs(c1),[],"all"), max(abs(c2),[],'all'), max(abs(c3),[],'all')]);
         threshold = p*T;
-        if contains(threshold_type, "hard")
-            [c1,I1] = Hard_threshold(threshold,c1);
-            [c2,I2] = Hard_threshold(threshold,c2);
-            [c3,I3] = Hard_threshold(threshold,c3);
+        if contains(threshold_type, "hard",'IgnoreCase',true)
+            [c1,~] = Hard_threshold(threshold,c1);
+            [c2,~] = Hard_threshold(threshold,c2);
+            [c3,~] = Hard_threshold(threshold,c3);
         else
-            [c1,I1] = Soft_threshold(threshold,c1);
-            [c2,I2] = Soft_threshold(threshold,c2);
-            [c3,I3] = Soft_threshold(threshold,c3);
+            [c1,~] = Soft_threshold(threshold,c1);
+            [c2,~] = Soft_threshold(threshold,c2);
+            [c3,~] = Soft_threshold(threshold,c3);
         end
         
         B1 = waverec2(c1, l1, wavelet);
-        B2 = waverec2(c2, l1, wavelet);
-        B3 = waverec2(c3, l1, wavelet);
+        B2 = waverec2(c2, l2, wavelet);
+        B3 = waverec2(c3, l3, wavelet);
     end
 end
 
---
+++ src/ex2point31.m
@@ -0,0 +1,142 @@
+% Use 1000 equispaced points on the interval [-1,1].
+t = linspace(-2, 2, 1000);
+
+% Sample a smooth function
+y = abs(t) .*(2+cos(t)) .* sign(t);
+% Try a non-smooth function also:
+% y = abs(t) .* exp(t)
+%% decontruct signal, question 2.1
+
+% Compute its wavelet transform, four levels deep, using the Daubechies 2
+% wavelet.
+Level = 2;
+[c,l] = wavedec(y, 2, 'db2');
+[c,l,a,b] = swt(y, Level, wavelet);
+% Visualize the coefficients on a logarithmic scale.
+% Try to explain what you see! Experiment with other wavelets and, again,
+% try to understand the different results.
+figure
+semilogy(abs(c));
+xlabel("$i$",Interpreter="latex");
+ylabel("$|c_i|$",Interpreter="latex");
+
+%% recontruct signal, task 2.2
+
+% apply wavelet transform on signal with added noise
+rng(42)
+epsilon = 1e-1;
+noise = epsilon*rand(size(y));
+ynoise = y + noise;
+[cnoise,lnoise] = wavedec(ynoise, 5, 'db2');
+
+figure
+plot(t,y)
+hold on
+plot(t,ynoise)
+hold off
+xlabel("$t$",Interpreter="latex");
+ylabel("function",Interpreter="latex");
+legend("$f(t)$","$f(t)+\epsilon \mathcal{U}(0,1)$",Interpreter="latex")
+
+% Find small coefficents and set them to zero.
+%T = max(abs(cnoise));
+%delta = 10e-4*T;
+delta = 1e-1;
+
+% change here what thresholding method wanted
+cnoiseInit = cnoise;
+[cnoise,I] = Hard_threshold(delta,cnoise);
+%[cnoise,I] = Soft_threshold(delta,cnoise);
+
+% How many coefficients did we put to zero:
+length(I)
+% out of a total of
+length(cnoise)
+% ratio put to 0
+(1-length(I)/length(cnoise))*100
+errCoeff = abs(c-cnoise);
+
+% coefficent error between real and thresholded noisy
+figure
+semilogy(errCoeff);
+xlabel("$i$",Interpreter="latex");
+ylabel("$|c_i-\hat{c}_i|$",Interpreter="latex");
+
+% Reconstruct the signal
+y2 = waverec(cnoise, lnoise, 'db2');
+% Plot the error on a logarithmic scale. Experiment with the threshold
+% above and see what the effect is
+bias = mean(noise); %mean of uniform distrubution
+%noiseMean = mean(abs(noise));
+err = abs(y-y2+bias);
+errTotal = norm(err)
+errTotalnoise = norm(noise)
+figure
+semilogy(t, err)
+hold on
+%semilogy(t, noise)
+yline(bias,Label="noise",Interpreter="latex")
+hold off
+xlabel("$t$",Interpreter="latex");
+ylabel("Errors",Interpreter="latex");
+legend("$|f(t_i)-\hat{f}(t_i)+mean(noise)|$",Interpreter="latex")
+
+%% Question 2.3
+
+x = linspace(-10,0,101);
+deltaList = 10.^x;
+errorList = zeros(size(deltaList));
+errorMat = zeros(length(t),length(deltaList))';
+F = zeros(length(t),length(deltaList))';
+errorCoeffList = zeros(size(deltaList));
+for i = 1:length(deltaList)
+    delta = deltaList(i);
+    %[cnoise,I] = Hard_threshold(delta,cnoiseInit);
+    [cnoise,I] = Soft_threshold(delta,cnoiseInit);
+    
+    errorCoeffList(i) = mse(c,cnoise);
+    % Reconstruct the signal
+    y2 = waverec(cnoise, lnoise, 'db2');
+    % Plot the error on a logarithmic scale. Experiment with the threshold
+    % above and see what the effect is
+    %noiseMean = mean(abs(noise));
+    F(i,:) = y2-bias;
+    errorMat(i,:) = abs(y-y2+bias);
+    %err = norm(abs(y-y2+bias));
+    err = mse(y,y2+bias);
+    errorList(i) = err;
+end
+[BestErr,index] = min(errorList);
+[BestErrCoeff,indexCoeff] = min(errorCoeffList);
+bestDelta = deltaList(indexCoeff)
+BestErrCoeff
+
+figure
+semilogy(t, errorMat(indexCoeff,:))
+hold on
+%semilogy(t, noise)
+yline(bias,Label="noise",Interpreter="latex")
+hold off
+xlabel("$t$",Interpreter="latex");
+ylabel("Errors",Interpreter="latex");
+legend("$|f(t_i)-\hat{f}(t_i)+mean(noise)|$",Interpreter="latex")
+
+figure
+plot(t,y)
+hold on
+plot(t,F(indexCoeff,:))
+hold off
+xlabel("$t$",Interpreter="latex");
+ylabel("function",Interpreter="latex");
+legend("$f(t)$","$\hat{f}(t)-mean(noise)$",Interpreter="latex")
+
+function [c,I] = Hard_threshold(delta, c)
+    I = find(abs(c) < delta);
+    c(I) = 0;
+end
+
+function [c, I] = Soft_threshold(delta,c)
+    I = find(abs(c) < delta);
+    c = sign(c).*(abs(c)-delta);
+    c(I) = 0;
+end
